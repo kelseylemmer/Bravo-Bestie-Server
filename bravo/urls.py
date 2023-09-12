8cd3fd1cd0a89f423789497377f2a1f2f7501118
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-09-11 20:22:36 -0400

    fixed login

--- bravo/urls.py
+++ bravo/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from bravoapi.views import EpisodeView, SeasonView, FranchiseView, register_user
+from bravoapi.views import EpisodeView, SeasonView, FranchiseView, register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'episodes', EpisodeView, 'episode')
@@ -15,6 +15,7 @@ router.register(r'franchises', FranchiseView, 'franchise')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', login_user),
     path('register', register_user),
-    path('', include(router.urls))
+    path('', include(router.urls)),  # Include the router's URLs
 ]
