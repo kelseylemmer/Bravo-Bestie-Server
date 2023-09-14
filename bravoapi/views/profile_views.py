a38dd601df79406d159ed37c45d755543e16426a
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-09-14 19:04:54 -0400

    edit profile

--- bravoapi/views/profile_views.py
+++ bravoapi/views/profile_views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from bravoapi.models import Franchise, Profile
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class ProfileView(ViewSet):
@@ -16,9 +17,7 @@ class ProfileView(ViewSet):
             Response -- JSON serialized profile
         """
 
-        if "current" in request.query_params:
-            profile = Profile.objects.get(user=request.auth.user.id)
-
+        profile = Profile.objects.get(pk=pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
 
@@ -29,17 +28,26 @@ class ProfileView(ViewSet):
             Response -- JSON serialized list of profiles
         """
         profiles = Profile.objects.all()
-        serialized = ProfileSerializer(profiles, many=True)
+
+        token = request.query_params.get("token", None)
+        if token:
+            user = Token.objects.get(key=token).user
+            profiles = Profile.objects.get(user=user)
+        serialized = ProfileSerializer(profiles)
         return Response(serialized.data, status=status.HTTP_200_OK)
 
-    def update(self, request, pk):
+    def update(token, request, pk):
         """handles PUT requests for updating a Profile"""
+
+        favorite_franchise = Franchise.objects.get(
+            pk=request.data['favorite_franchise'])
+
         profile = Profile.objects.get(pk=pk)
-        profile.user = User.objects.get(pk=request.data["user"])
+        profile.user = request.auth.user
         profile.display_name = request.data["display_name"]
         profile.bio = request.data["bio"]
         profile.picture = request.data["picture"]
-        profile.favorite_franchise = request.data["favorite_franchise"]
+        profile.favorite_franchise = favorite_franchise
 
         profile.save()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
