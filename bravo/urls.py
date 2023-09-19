4a11b9d4581b7c15e5c18363ed519c241d1a2585
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-09-18 20:36:06 -0400

    profile_episode view for profile and for franchise details

--- bravo/urls.py
+++ bravo/urls.py
@@ -6,14 +6,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from bravoapi.views import EpisodeView, SeasonView, FranchiseView, register_user, login_user, ProfileEpisodeView, ProfileView
+from bravoapi.views import EpisodeView, SeasonView, FranchiseView, register_user, login_user, ProfileProfileEpisodeView, ProfileView, FranchiseProfileEpisodeView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'episodes', EpisodeView, 'episode')
 router.register(r'seasons', SeasonView, 'season')
 router.register(r'franchises', FranchiseView, 'franchise')
-router.register(r'profileEpisodes', ProfileEpisodeView, 'profileEpisode')
+router.register(r'profileEpisodes',
+                ProfileProfileEpisodeView, 'profileEpisode')
 router.register(r'profiles', ProfileView, 'profile')
+router.register(r'profileEpisode',
+                FranchiseProfileEpisodeView, 'profile episode')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- bravoapi/views/__init__.py
+++ bravoapi/views/__init__.py
@@ -3,5 +3,6 @@ from .episode_views import EpisodeView
 from .season_views import SeasonView
 from .franchise_views import FranchiseView
 from .auth import register_user, login_user
-from .profile_episode_views import ProfileEpisodeView
 from .profile_views import ProfileView
+from .profile_episodes_view_p import ProfileProfileEpisodeView
+from .profile_episode_views_f import FranchiseProfileEpisodeView
