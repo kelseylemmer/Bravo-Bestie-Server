4f43923d3c19548974398f4ef45fe9f9e868f379
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-09-20 19:53:20 -0400

    book fixtures

--- bravoapi/models/book.py
+++ bravoapi/models/book.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     """Book model class"""
 
     title = models.CharField(max_length=40)
+    pages = models.IntegerField()
     synopsis = models.CharField(max_field=1000)
     publisher = models.CharField(max_field=50)
     img_url = models.URLField()
