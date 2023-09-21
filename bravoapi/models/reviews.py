33cf51f509d396e600e490be16686be010d5d918
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-09-21 03:16:28 -0400

    profile and user fixtures, review model

---
+++ bravoapi/models/reviews.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Review(models.Model):
+    """ProfileReview model class"""
+
+    profile = models.ForeignKey(
+        'Profile', on_delete=models.CASCADE)
+    review = models.CharField(max_length=1000)
+    date = models.DateTimeField()
--- seed_database.sh
+++ seed_database.sh
@@ -9,11 +9,13 @@ python3 manage.py loaddata franchises
 python3 manage.py loaddata seasons
 python3 manage.py loaddata episodes
 python3 manage.py loaddata users
+python3 manage.py loaddata profiles
 python3 manage.py loaddata tokens
 python3 manage.py loaddata cast
 python3 manage.py loaddata roles
 python3 manage.py loaddata season_cast
 python3 manage.py loaddata franchise_cast
 python3 manage.py loaddata books
+
 
 python3 manage.py loaddata profile_episode
